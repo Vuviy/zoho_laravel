b643483527228bf11d13d11b5b7391ed696bdb21
Vuviy <89077737+Vuviy@users.noreply.github.com>
2022-02-04 15:59:11 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-//use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
-//use Symfony\Component\HttpFoundation\Request;
 use Illuminate\Http\Request;
-
-
-//use App\Http\Controllers\Conti;
-
-
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
 
 Route::get('/', function () {
     return view('home');
@@ -25,7 +14,7 @@ Route::get('/deal', function () {
     return view('deal');
 })->name('deal');
 
-Route::post('/add-deal', 'App\Http\Controllers\AddDealController@second')->name('add-deal');
+Route::post('/add-deal', 'App\Http\Controllers\AddDealController@addDeal')->name('add-deal');
 
 
 Route::get('/tusk', function () {
